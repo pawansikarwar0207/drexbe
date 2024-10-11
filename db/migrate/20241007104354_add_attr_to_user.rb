cbf9e4e5c04fc71e329f7e07a4ca0675decb24a5
pawansikarwar0207 <pawansikarwar96@gmail.com>
2024-10-11 18:43:55 +0530

    feature/add-home-page-and-address

--- db/migrate/20241007104354_add_attr_to_user.rb
+++ db/migrate/20241007104354_add_attr_to_user.rb
@@ -4,5 +4,7 @@ class AddAttrToUser < ActiveRecord::Migration[7.0]
     add_column :users,:confirmed_at, :datetime
     add_column :users,:confirmation_sent_at, :datetime
     add_column :users,:unconfirmed_email, :string
+    add_column :users,:address_1, :string
+    add_column :users,:address_2, :string
   end
 end
