9a85e0b72b771ec008080ca5190685be14d4834b
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-02-03 18:11:09 +0530

    feature/remove the stimulus JS for traveler form

--- app/javascript/controllers/form_steps_controller.js
+++ app/javascript/controllers/form_steps_controller.js
@@ -1,35 +1,37 @@
-import { Controller } from "@hotwired/stimulus";
+// import { Controller } from "@hotwired/stimulus";
 
-export default class extends Controller {
-  static targets = ["step1", "step2", "backIcon"];
+// export default class extends Controller {
+//   static targets = ["step1", "step2", "backIcon", "submitModal"];
 
-  connect() {
-    console.log("FormStepsController connected");
-  }
+//   connect() {
+//     console.log("FormStepsController connected");
+//   }
 
-  next(event) {
-    event.preventDefault();
-    this.step1Target.style.display = "none";
-    this.step2Target.style.display = "block";
+//   next(event) {
+//     event.preventDefault();
+//     this.step1Target.style.display = "none";
+//     this.step2Target.style.display = "block";
 
-    // Change back arrow to cross icon and enable modal
-    this.backIconTarget.innerHTML = '<i class="fas fa-times"></i>';
-    this.backIconTarget.setAttribute("data-bs-toggle", "modal");
-    this.backIconTarget.setAttribute("data-bs-target", "#abandonModal");
-  }
+//     // Change back arrow to cross icon and enable modal
+//     this.backIconTarget.innerHTML = '<i class="fas fa-times"></i>';
+//     this.backIconTarget.setAttribute("data-bs-toggle", "modal");
+//     this.backIconTarget.setAttribute("data-bs-target", "#abandonModal");
+//   }
 
-  prev(event) {
-    event.preventDefault();
-    this.step1Target.style.display = "block";
-    this.step2Target.style.display = "none";
+//   prev(event) {
+//     event.preventDefault();
+//     this.step1Target.style.display = "block";
+//     this.step2Target.style.display = "none";
 
-    // Change cross icon back to arrow
-    this.backIconTarget.innerHTML = '<i class="fas fa-chevron-left"></i>';
-    this.backIconTarget.removeAttribute("data-bs-toggle");
-    this.backIconTarget.removeAttribute("data-bs-target");
-  }
+//     // Change cross icon back to arrow
+//     this.backIconTarget.innerHTML = '<i class="fas fa-chevron-left"></i>';
+//     this.backIconTarget.removeAttribute("data-bs-toggle");
+//     this.backIconTarget.removeAttribute("data-bs-target");
+//   }
 
-  abandon() {
-    window.location.href = "/";
-  }
-}
+//   abandon() {
+//     window.location.href = "/";
+//     let modal = bootstrap.Modal.getInstance(document.getElementById("abandonModal"));
+//     if (modal) modal.hide();
+//   }
+// }
